8d80ab96cc6597d0da3b3af46b0f16001351e148
Justin Thomas <justin@jdt.io>
2023-12-14 05:02:05 +0000

    tweak return

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use pyo3::prelude::*;
-use pyo3::types::IntoPyDict;
+use pyo3::types::{IntoPyDict, PyList};
 
 pub fn poc() {
     let x = Python::with_gil(|py| {
         let platform = PyModule::import(py, "sonic_platform.platform").unwrap();
-        let psus: &PyAny = platform
+        let psus: &PyList = platform
             .call_method0("Platform").unwrap()
             .call_method0("get_chassis").unwrap()
-            .call_method0("get_all_psus").unwrap();
+            .call_method0("get_all_psus").unwrap()
+            .extract::<&PyList>().unwrap();
 
         println!("{psus:#?}");
     });
